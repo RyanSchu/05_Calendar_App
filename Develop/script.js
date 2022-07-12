26dda49964594669faac539fc9650651fa65e44c
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-07-11 22:56:55 -0500

    commit starter code

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+
+var dateHeader = document.querySelector("#currentDay")
+var container = document.querySelector(".container")
+var current = moment()
+var timePoints = 
+
+dateHeader.textContent = current.format("MMMM Do, YYYY")
+
+
