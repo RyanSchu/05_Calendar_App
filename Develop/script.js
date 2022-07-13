50b11dd368cf00efc9f5ed19c09ea9849bbe4b45
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-07-13 00:10:06 -0500

    added local storage functions

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var container = document.querySelector(".container")
 var current = moment()
 var currentHour = moment(current.format("h a"),"h a")
 class Calendar {
-    constructor(am9,am10,am11,pm12,pm1,pm2,pm3,pm4,pm5) {
+    constructor(am9=null,am10=null,am11=null,pm12=null,pm1=null,pm2=null,pm3=null,pm4=null,pm5=null) {
         this.am9 = am9,
         this.am10= am10,
         this.am11 = am11,
@@ -22,7 +22,7 @@ function getLocalStorage() {
     if (localStorage.getItem("calendarObject") === null) {
         return new Calendar()
     } else {
-        return localStorage.getItem("calendarObject")
+        return JSON.parse(localStorage.getItem("calendarObject"))
     }
 }
 
@@ -30,15 +30,19 @@ function renderTimeBlocks(calendarObject) {
     let keys=Object.keys(calendarObject)
     for ( i = 0; i < keys.length; i++) {
         //  row contains timeblock which contains all of hour, description, and save button
-        let row=document.createElement("div")
+        let row=document.createElement("form")
         row.setAttribute("class","row timeblock w-100")
         let hour=document.createElement("div")
         hour.textContent = "AM"
         hour.setAttribute("class","hour col-1")
         let description = document.createElement("textarea")
         description.setAttribute("class","description col-10 " + checkRelativeToNow(keys[i]))
+        description.value = calendarObject[keys[i]]
+        description.setAttribute("name",keys[i])
         let button = document.createElement("button")
         button.setAttribute("class","saveBtn col-1")
+        button.setAttribute("name",keys[i])
+        button.addEventListener("click",saveContent)
         row.appendChild(hour)
         row.appendChild(description)
         row.appendChild(button)
@@ -62,6 +66,14 @@ function checkRelativeToNow(time) {
     } else {
         return "present"
     }
+}
+
+function saveContent(event) {
+    event.preventDefault()
+    let trueTarget = document.querySelectorAll(`textarea[name=${event.target.name}]`)[0]
+    console.log(trueTarget)
+    calendarObject[event.target.name] = trueTarget.value
+    localStorage.setItem("calendarObject",JSON.stringify(calendarObject))
 }
 
 dateHeader.textContent = current.format("MMMM Do, YYYY")
